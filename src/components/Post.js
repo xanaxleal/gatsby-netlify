d748faefd6ea39a77a5bd3b28b4ae4b6b12d3adf
Ana Leal <leal@MacBook-Pro-de-Ana.local>
2020-12-28 14:48:18 +0100

    excerpt fix

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,9 @@ const Post = (props) => (
             <Card.Img variant="top" src={props.image} alt={props.alt || "default"} />
             <Card.Body>
                 <Card.Title dangerouslySetInnerHTML={{__html: props.title}} />
-                <Card.Text dangerouslySetInnerHTML={{__html: props.excerpt}} />
+                <Card.Text>
+                    <div dangerouslySetInnerHTML={{__html: props.excerpt}} />
+                </Card.Text>
                 <Button variant="warning" href={props.readMore}>Read more</Button>
             </Card.Body>
         </Card>  
